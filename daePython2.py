dfff4d653d55ffdc252b111fec6c2edce8c60cae
Sa64 <sa64@Skills-Academy-64.local>
2024-06-04 17:37:21 -0400

    Added named constants for the filenames

--- daePython2.py
+++ daePython2.py
@@ -1,12 +1,14 @@
 import json
 
+FILENAME = 'holder.json'
+
 def addPassword():
     print('Username:')
     username = input()
     print('Password')
     password = input()
     try:
-        with open('holder.json', 'r') as openfile:
+        with open(FILENAME, 'r') as openfile:
             json_object = json.load(openfile)
     except:
         json_object = {}
@@ -14,19 +16,19 @@ def addPassword():
         print('File Opened')
     finally:
         json_object.update({username: password})
-        with open("holder.json", "w") as outfile:
+        with open(FILENAME, "w") as outfile:
             json.dump(json_object, outfile)
 
 
 def getPassword():
-    with open('holder.json', 'r') as openfile:
+    with open(FILENAME, 'r') as openfile:
         json_object = json.load(openfile)
     print('Input username for the password you want to access')
     username = input()
     for x in json_object.keys():
         if x == username:
             return f'Username: {username}, Password: {json_object[username]}'
-    return 'Username not foundcr'
+    return 'Username not foundcrF'
     
 
 def main():
