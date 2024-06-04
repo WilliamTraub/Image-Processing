5a962c9dd8d2155085a321ca33ae2833b8cf6386
Sa64 <sa64@Skills-Academy-64.local>
2024-06-04 17:28:28 -0400

    Create a password manager file. First push

---
+++ daePython2.py
@@ -0,0 +1,46 @@
+import json
+
+def addPassword():
+    print('Username:')
+    username = input()
+    print('Password')
+    password = input()
+    try:
+        with open('holder.json', 'r') as openfile:
+            json_object = json.load(openfile)
+    except:
+        json_object = {}
+    else:
+        print('File Opened')
+    finally:
+        json_object.update({username: password})
+        with open("holder.json", "w") as outfile:
+            json.dump(json_object, outfile)
+
+
+def getPassword():
+    with open('holder.json', 'r') as openfile:
+        json_object = json.load(openfile)
+    print('Input username for the password you want to access')
+    username = input()
+    for x in json_object.keys():
+        if x == username:
+            return f'Username: {username}, Password: {json_object[username]}'
+    return 'Username not foundcr'
+    
+
+def main():
+    empty_dict = {}
+    
+    while True:
+        print('Input "create" to create a new password or "read" to recover an existing password. Input "end" to end')
+        selection = input()
+        if selection == 'create':
+            addPassword()
+        elif selection == 'read':
+            print(getPassword())
+        elif selection == 'end':
+            break
+        else:
+            print('Misunderstood input')
+main()
